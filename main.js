d458ebebbe7266b2b9b7663f81196607ee00ad5c
mmilic1994 <mmilic1994@gmail.com>
2018-12-20 22:12:20 +0100

    some stuff

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const current = document.getElementById('current-item');current.addEventListener('click', function() {
+  const current = document.getElementById('current-item'); current.addEventListener('click', function () {
     current.parentNode.classList.toggle('exp');
   });
 
   const menu = document.getElementById('menu');
-  menu.addEventListener('mouseleave', function() {
+  menu.addEventListener('mouseleave', function () {
     menu.classList.remove('exp');
   });
 
   let name = document.getElementById('name').value;
-  let submit = document.getElementById('submit');
-  submit.addEventListener('click', ()=> {
+  window.addEventListener('load', () => {
     let message = document.querySelector('.thanks1');
     message.innerHTML = `<h2>Thank you ${name} for getting in touch!</h2>`;
   });
