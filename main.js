c0d7e10c8567028159f517f71697f23398b72133
mmilic1994 <mmilic1994@gmail.com>
2018-12-20 22:02:06 +0100

    some JS

--- main.js
+++ main.js
@@ -7,5 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
   menu.addEventListener('mouseleave', function() {
     menu.classList.remove('exp');
   });
+
+  let name = document.getElementById('name').value;
+  let submit = document.getElementById('submit');
+  submit.addEventListener('click', ()=> {
+    let message = document.querySelector('.thanks1');
+    message.innerHTML = `<h2>Thank you ${name} for getting in touch!</h2>`;
+  });
 });
 
