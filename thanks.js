440e0c541c71dc13da125679feec6f28af886011
mmilic1994 <mmilic1994@gmail.com>
2018-12-20 22:29:14 +0100

    final changes

--- thanks.js
+++
@@ -1,5 +0,0 @@
-let name = document.getElementById('name').value;
-  window.addEventListener('load', () => {
-    let message = document.querySelector('.thanks1');
-    message.innerHTML = `<h2>Thank you ${name} for getting in touch!</h2>`;
-  });
